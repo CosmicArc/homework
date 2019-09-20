1b214c0d0e9fe9508b86f5f9835761eafbd1678b
CosmicArc <55443303+CosmicArc@users.noreply.github.com>
2019-09-20 08:47:03 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/mian.cpp
@@ -0,0 +1,21 @@
+//Name: Nathaniel Sanchez
+//Intro to compuuter science
+//period 1
+//Mr.williams
+//2019-09-19 Classwork
+
+#include<iostream>
+int main()
+{
+	std::cout << "Nathaniel Sanchez" << std::endl 
+		std::cout << "Period 1 Computer science" << std::endl
+		std::cout << "Periiod 2 Health" << std::endl
+		std::cout << "Period 3 Geometry" << std::endl
+		std::cout << "Period 4 House" << std::endl
+		std::cout << "Period 5 Spanish" << std::endl
+		std::cout << "Period 6 Ela" << std::endl
+		std::cout << "Period 7 Lunch" << std::endl
+		std::cout << "Period 8 Earth science" << std::endl
+		std::cout << "Period 9 Gym" << std::endl
+		std::cout << "Perio dGlobal" << std::endl;
+}
